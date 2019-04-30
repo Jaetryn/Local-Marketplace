19cdf111ea60688c113e49491bb4b161d31ac7c0
bbb267 <bwbellis@yahoo.com>
2019-04-30 16:23:43 -0400

    Added Firebase integration and item model

---
+++ LocalMarketplace/LocalMarketplace/ItemModel.swift
@@ -0,0 +1,67 @@
+//
+//  ItemModel.swift
+//  SemesterProject
+//
+//  Created by Ben Bellis on 4/19/19.
+//  Copyright © 2019 Ben Bellis. All rights reserved.
+//
+
+import Foundation
+import Firebase
+import UIKit
+
+// Messing around, could add this
+enum ItemQuality: String {
+    case new = "New"
+    case slightlyUsed = "Slightly Used"
+    case moderatelyUsed = "Moderately Used"
+    case damaged = "Damaged"
+}
+
+class Item {
+    var name: String
+    var price: String
+    var itemDescription: String
+    var quality: String
+    var image: UIImage?
+    var imagePath: String?
+    var location: String? // TEMPORARY, will add location support
+    var owner: String
+    var ref: DatabaseReference?
+    
+    init(name: String, price: String, quality: String, desc: String, img: UIImage?) {
+        self.name = name
+        self.price = price
+        self.itemDescription = desc
+        self.quality = quality
+        image = img
+        location = "here"
+        owner = "me"
+    }
+    
+    init(snapshot: DataSnapshot) {
+        let data = snapshot.value as! Dictionary<String, Any>
+        if let name = data["name"] as? String {
+            self.name = name
+        } else { self.name = "" }
+        if let price = data["price"] as? String {
+            self.price = price
+        } else { self.price = "" }
+        if let desc = data["description"] as? String {
+            self.itemDescription = desc
+        } else { self.itemDescription = "" }
+        if let owner = data["owner"] as? String {
+            self.owner = owner
+        } else { self.owner = "" }
+        if let quality = data["quality"] as? String {
+            self.quality = quality
+        } else { self.quality = "" }
+        //        if let image = data["image"] as? UIImage {
+        //            self.image = image
+        //        } else { image = nil }
+        
+        location = "here"
+        
+        ref = snapshot.ref
+    }
+}
